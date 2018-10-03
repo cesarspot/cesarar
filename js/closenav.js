567b665c6d63f2db43ed720a0479892d1b90e32f
cesarspot <rojascesar443@gmail.com>
2018-10-03 14:28:13 -0400

    mi primer portafolio

---
+++ js/closenav.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$(".closenav").click(function () {
+			$('.navbar-toggler').click();
+	});
+});
---
+++ js/main.js
@@ -0,0 +1,76 @@
+//------------------------------------
+//TODOS LOS SCRIPTS
+//------------------------------------
+//CHANGE COLOR NAV
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 50) {
+      $(".principal").css("background","#292c2f");
+    }
+    else {
+      $(".principal").css("background","transparent");  
+    }
+  })
+})
+//CHANGE COLOR NAV
+
+//------------------------------------
+//BOTON IR ARRIBA
+//------------------------------------
+
+//script para que funcione al 100% el botón ir arriba 
+
+//invocamos al objeto (window) y a su método (scroll), solo se ejecutara si el usuario hace scroll en la página
+$(window).scroll(function(){
+    if($(this).scrollTop() > 300){ //condición a cumplirse cuando el usuario aya bajado 301px a más.
+      $("#js_up").slideDown(500); //se muestra el botón en 300 mili segundos
+    }else{ // si no
+      $("#js_up").slideUp(500); //se oculta el botón en 300 mili segundos
+    }
+  });
+  
+  //creamos una función accediendo a la etiqueta i en su evento click
+  $("#js_up i").on('click', function (e) { 
+    e.preventDefault(); //evita que se ejecute el tag ancla (<a href="#">valor</a>).
+    $("body,html").animate({ // aplicamos la función animate a los tags body y html
+      scrollTop: 0 //al colocar el valor 0 a scrollTop me volverá a la parte inicial de la página
+    },700); //el valor 700 indica que lo ara en 700 mili segundos
+    return false; //rompe el bucle
+  });
+//BOTON IR ARRIBA
+
+//------------------------------------
+//LOADER
+//------------------------------------
+$(window).onload(function() {
+    var contenedor = document.getElementById('contenedor_loader');
+    contenedor.style.visibility = 'hidden';
+    contenedor.style.opacity = '0';
+    
+});
+//LOADER
+
+//------------------------------------
+//SLIDE ANIMATION
+//------------------------------------
+$(document).ready(function(){
+    $(window).scroll(function() {
+     $(".slideanim").each(function(){
+       var pos = $(this).offset().top;
+   
+       var winTop = $(window).scrollTop();
+         if (pos < winTop + 600) {
+           $(this).addClass("slide");
+         }
+       });
+     });
+   })
+//SLIDE ANIMATION
+
+//------------------------------------
+/*TOGGLE ICON NAVBAR*/
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+/*TOGGLE ICON NAVBAR*/
